5a06d129c1a4f2a64a6aebfb26a1c2af99d3f437
jdaraver2011 <76464203+jdaraver2011@users.noreply.github.com>
2020-12-21 23:14:28 -0600

    Add files via upload

---
+++ FIX_SC_AUTOMATE.ps1
@@ -0,0 +1,32 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+$HOST.UI.RawUI.ForegroundColor = "Green"
+Write-Host "-----------------------------------------------------------------"
+Write-Host "|***Uninstall/Reinstall of Connectwise ScreenConnect/Automate***|"
+Write-Host "-----------------------------------------------------------------"
+Write-Host 
+$locationId = Read-Host -prompt 'What location would you like to install? If not known type leave blank for ProTEK New Computers'
+Write-Host
+Write-Host "---------------------------------------------------"
+Write-Host "|Step 1: Uninstall of Connectwise ScreenConnect***|"
+Write-Host "---------------------------------------------------"
+$MyApp = Get-WmiObject -Class Win32_Product | Where-Object {$_.Vendor -Match "ScreenConnect"}
+$MyApp.Uninstall()
+If ($null -eq $Input) {$Input = '1'}
+Write-Host
+Write-Host "----------------------------------------------"
+Write-Host "|Step 2: Uninstall of Connectwise Automate***|"
+Write-Host "----------------------------------------------"
+Write-Host
+Invoke-Expression(New-Object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Uninstall-LTService -Server https://numsp.hostedrmm.com
+Write-Host
+Write-Host "------------------------------------------------------------------------"
+Write-Host "|Step 3: Install of Connectwise Automate to location specified above***|"
+Write-Host "------------------------------------------------------------------------"
+Write-Host
+Invoke-Expression(New-Object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Install-LTService -Server https://proteksolutions.hostedrmm.com -Password MiIbitsBh26O65C58rTLR3ehQ4vPzX37 -LocationID $locationID
+pause

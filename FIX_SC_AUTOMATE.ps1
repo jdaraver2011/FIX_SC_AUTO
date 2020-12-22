4d519e12be9ee4a1dd4bcb8c5800778b4245ccdd
jdaraver2011 <76464203+jdaraver2011@users.noreply.github.com>
2020-12-22 13:23:54 -0600

    Update FIX_SC_AUTOMATE.ps1

--- FIX_SC_AUTOMATE.ps1
+++ FIX_SC_AUTOMATE.ps1
@@ -1,9 +1,9 @@
 $HOST.UI.RawUI.ForegroundColor = "Green"
-Write-Host "-----------------------------------------------------------------"
-Write-Host "|***Uninstall/Reinstall of Connectwise ScreenConnect/Automate***|"
-Write-Host "-----------------------------------------------------------------"
+Write-Host "--------------------------------------------------"
+Write-Host "|Cleanup of Connectwise ScreenConnect/Automate***|"
+Write-Host "--------------------------------------------------"
 Write-Host 
-$locationId = Read-Host -prompt 'What location would you like to install? If not known type leave blank for ProTEK New Computers'
+$locationId = Read-Host -prompt 'Location to install? Leave blank for ProTEK New Computers'
 Write-Host
 Write-Host "---------------------------------------------------"
 Write-Host "|Step 1: Uninstall of Connectwise ScreenConnect***|"
@@ -18,9 +18,9 @@ Write-Host "----------------------------------------------"
 Write-Host
 Invoke-Expression(New-Object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Uninstall-LTService -Server https://numsp.hostedrmm.com
 Write-Host
-Write-Host "------------------------------------------------------------------------"
-Write-Host "|Step 3: Install of Connectwise Automate to location specified above***|"
-Write-Host "------------------------------------------------------------------------"
+Write-Host "--------------------------------------------"
+Write-Host "|Step 3: Install of Connectwise Automate***|"
+Write-Host "--------------------------------------------"
 Write-Host
 Invoke-Expression(New-Object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Install-LTService -Server https://proteksolutions.hostedrmm.com -Password MiIbitsBh26O65C58rTLR3ehQ4vPzX37 -LocationID $locationID
 pause

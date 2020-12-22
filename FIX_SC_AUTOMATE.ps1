d7caaeb113ea0f292adf36a7d2cf4e11689586c5
jdaraver2011 <76464203+jdaraver2011@users.noreply.github.com>
2020-12-22 12:27:52 -0600

    Update FIX_SC_AUTOMATE.ps1

--- FIX_SC_AUTOMATE.ps1
+++ FIX_SC_AUTOMATE.ps1
@@ -1,5 +1,3 @@
-Function Install-Automate{
-<#
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
 {  
   $arguments = "& '" +$myinvocation.mycommand.definition + "'"

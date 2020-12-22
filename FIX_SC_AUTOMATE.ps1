b11c60b00b1c4f78763f0a481ba290dd5c64472f
jdaraver2011 <76464203+jdaraver2011@users.noreply.github.com>
2020-12-22 11:38:34 -0600

    Update FIX_SC_AUTOMATE.ps1

--- FIX_SC_AUTOMATE.ps1
+++ FIX_SC_AUTOMATE.ps1
@@ -1,4 +1,4 @@
-Function Get-LTServiceInfo{
+Function Install-Automate{
 <#
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
 {  
